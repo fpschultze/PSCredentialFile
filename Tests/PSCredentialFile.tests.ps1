c9ec340ca4c325708d3aff83bca4cd7fc064592a
Frank Peter Schultze <fpschultze@live.de>
2017-01-26 16:56:32 +0100

    Use TESTDRIVE

--- Tests/PSCredentialFile.tests.ps1
+++ Tests/PSCredentialFile.tests.ps1
@@ -2,24 +2,30 @@
 $ModuleFile = (Split-Path -Leaf $PSCommandPath) -replace '\.tests\.ps1$', '.psm1'
 Import-Module "$ModuleRoot\$ModuleFile"
 
-$TestFile = [System.IO.Path]::GetTempFileName()
-$TestCred = New-Object pscredential 'foo\bar', (ConvertTo-SecureString 'foobar' -AsPlainText -Force)
-if (Test-Path -Path $TestFile) {Remove-Item -Path $TestFile}
-
 Describe 'PSCredentialFile Module' {
 
+  $TestCred = New-Object pscredential 'foo\bar', (ConvertTo-SecureString 'foO!b4r' -AsPlainText -Force)
+  $TestFile = 'TESTDRIVE:\cred.xml'
+
   Context 'Running New-PSCredentialFile without PassThru switch' {
+
     It 'creates credential file' {
       New-PSCredentialFile -Path $TestFile -Credentials $TestCred | Should BeNullOrEmpty
       $TestFile | Should Exist
     }
   }
+
   Context 'Running New-PSCredentialFile with PassThru switch' {
+
     It 'additionally returns credential object' {
       New-PSCredentialFile -Path $TestFile -Credentials $TestCred -PassThru | Should Be $TestCred
     }
   }
+
   Context 'Running Import-PSCredentialFile' {
+
+    New-PSCredentialFile -Path $TestFile -Credentials $TestCred
+
     It 'returns pscredential object' {
       Import-PSCredentialFile -Path $TestFile | Should BeOfType pscredential
     }
